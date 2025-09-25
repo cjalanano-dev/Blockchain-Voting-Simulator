e994b84a9baef2c0b4d0e136ca4f920e09581ec9
Carlos James <carlosalanano.dev@gmail.com>
2025-09-25 15:14:05 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return(
     <div className="App">
-      <h1 className="text-3xl font-bold">Hello, World!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
-  )
+  )  
 }
 
 export default App
